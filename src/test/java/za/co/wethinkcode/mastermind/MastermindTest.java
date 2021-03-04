3e7e4b6c6a6e4e5ccae8f95dddebc312a19489e2
Waxes27 <nceba.dumasi@gmail.com>
2021-03-04 19:55:26 +0200

    wtc-lms save

--- src/test/java/za/co/wethinkcode/mastermind/MastermindTest.java
+++ src/test/java/za/co/wethinkcode/mastermind/MastermindTest.java
@@ -3,13 +3,7 @@ package za.co.wethinkcode.mastermind;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
-import org.mockito.internal.matchers.Any;
-import za.co.wethinkcode.mastermind.CodeGenerator;
-import za.co.wethinkcode.mastermind.Mastermind;
-import za.co.wethinkcode.mastermind.Player;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -44,198 +38,164 @@ public class MastermindTest {
         Mastermind game = new Mastermind(new CodeGenerator(mockedNumber),new Player(mockedInputStream));
         game.runGame();
 
-        assertEquals("""
-                4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 3
-                Turns left: 11
-                Input 4 digit code:
-                Number of correct digits in correct place: 4
-                Number of correct digits not in correct place: 0
-                Congratulations! You are a codebreaker!
-                The code was: 2345""",capturedOutput.toString().strip());
+        assertEquals("4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "Turns left: 11\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 4\n"+
+                "Number of correct digits not in correct place: 0\n"+
+                "Congratulations! You are a codebreaker!\n"+
+                "The code was: 2345",capturedOutput.toString().strip());
 
     }
     @Test
     public void testOutputIncorrect(){
         when(mockedNumber.nextInt(anyInt())).thenReturn(1,2,3,4);
 
-        InputStream mockedInput = new ByteArrayInputStream("""
-                3456
-                4567
-                5678
-                6788
-                1122
-                2233
-                3344
-                4455
-                5566
-                6677
-                8888
-                1234
-                """.getBytes(StandardCharsets.UTF_8));
+        InputStream mockedInput = new ByteArrayInputStream("3456\n4567\n5678\n6788\n1122\n2233\n3344\n4455\n5566\n6677\n3344\n1234\n".getBytes(StandardCharsets.UTF_8));
 
         Mastermind game = new Mastermind(new CodeGenerator(mockedNumber),new Player(mockedInput));
         game.runGame();
 
-        assertEquals("""
-                4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 3
-                Turns left: 11
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 2
-                Turns left: 10
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 1
-                Turns left: 9
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 0
-                Turns left: 8
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 2
-                Turns left: 7
-                Input 4 digit code:
-                Number of correct digits in correct place: 1
-                Number of correct digits not in correct place: 3
-                Turns left: 6
-                Input 4 digit code:
-                Number of correct digits in correct place: 2
-                Number of correct digits not in correct place: 2
-                Turns left: 5
-                Input 4 digit code:
-                Number of correct digits in correct place: 1
-                Number of correct digits not in correct place: 3
-                Turns left: 4
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 2
-                Turns left: 3
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 0
-                Turns left: 2
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 0
-                Turns left: 1
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 3
-                No more turns left.
-                The code was: 2345""",capturedOutput.toString().trim());
+        assertEquals("4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it."+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "Turns left: 11\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 10\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 1\n"+
+                "Turns left: 9\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 0\n"+
+                "Turns left: 8\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 7\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 1\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "Turns left: 6\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 2\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 5\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 1\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "Turns left: 4\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 3\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 0\n"+
+                "Turns left: 2\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 0\n"+
+                "Turns left: 1\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "No more turns left.\n"+
+                "The code was: 2345",capturedOutput.toString().trim());
     }
     @Test
     public void testLettersThenCorrect() {
         when(mockedNumber.nextInt(anyInt())).thenReturn(1,2,3,4);
-        InputStream mockedInputStream = new ByteArrayInputStream("""
-                qw12\n23er\n34Ed\nqwer\n2345\n
-                """.getBytes(StandardCharsets.UTF_8));
+        InputStream mockedInputStream = new ByteArrayInputStream("qw12\n23er\n34Ed\nqwer\n2345\n".getBytes(StandardCharsets.UTF_8));
 
         Mastermind game = new Mastermind(new CodeGenerator(mockedNumber),
                 new Player(mockedInputStream));
         game.runGame();
 
-        assertEquals("""
-                4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.
-                Input 4 digit code:
-                Please enter exactly 4 digits (each from 1 to 8).
-                Input 4 digit code:
-                Please enter exactly 4 digits (each from 1 to 8).
-                Input 4 digit code:
-                Please enter exactly 4 digits (each from 1 to 8).
-                Input 4 digit code:
-                Please enter exactly 4 digits (each from 1 to 8).
-                Input 4 digit code:
-                Number of correct digits in correct place: 4
-                Number of correct digits not in correct place: 0
-                Congratulations! You are a codebreaker!
-                The code was: 2345""",capturedOutput.toString().strip());
+        assertEquals("4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.\n"+
+                "Input 4 digit code:\n"+
+                "Please enter exactly 4 digits (each from 1 to 8).\n"+
+                "Input 4 digit code:\n"+
+                "Please enter exactly 4 digits (each from 1 to 8).\n"+
+                "Input 4 digit code:\n"+
+                "Please enter exactly 4 digits (each from 1 to 8).\n"+
+                "Input 4 digit code:\n"+
+                "Please enter exactly 4 digits (each from 1 to 8).\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 4\n"+
+                "Number of correct digits not in correct place: 0\n"+
+                "Congratulations! You are a codebreaker!\n"+
+                "The code was: 2345",capturedOutput.toString().strip());
     }
     @Test
     public void testLettersThenIncorrect(){
         when(mockedNumber.nextInt(anyInt())).thenReturn(1,2,3,4);
-        InputStream mockedInputStream = new ByteArrayInputStream("""
-                qw23\nQw34
-                1234
-                3456
-                4567
-                5687
-                2435
-                1324
-                4657
-                8567
-                4455
-                1122
-                2233
-                3344
-                
-                """.getBytes(StandardCharsets.UTF_8));
+        InputStream mockedInputStream = new ByteArrayInputStream("qw23\nQw34\n1234\n3456\n4567\n5687\n2435\n1324\n4657\n8567\n4455\n1122\n2233\n3344\n".getBytes(StandardCharsets.UTF_8));
         Mastermind game = new Mastermind(new CodeGenerator(mockedNumber),
                 new Player(mockedInputStream));
         game.runGame();
 
-        assertEquals("""
-                4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.
-                Input 4 digit code:
-                Please enter exactly 4 digits (each from 1 to 8).
-                Input 4 digit code:
-                Please enter exactly 4 digits (each from 1 to 8).
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 3
-                Turns left: 11
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 3
-                Turns left: 10
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 2
-                Turns left: 9
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 1
-                Turns left: 8
-                Input 4 digit code:
-                Number of correct digits in correct place: 2
-                Number of correct digits not in correct place: 2
-                Turns left: 7
-                Input 4 digit code:
-                Number of correct digits in correct place: 1
-                Number of correct digits not in correct place: 2
-                Turns left: 6
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 2
-                Turns left: 5
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 1
-                Turns left: 4
-                Input 4 digit code:
-                Number of correct digits in correct place: 1
-                Number of correct digits not in correct place: 3
-                Turns left: 3
-                Input 4 digit code:
-                Number of correct digits in correct place: 0
-                Number of correct digits not in correct place: 2
-                Turns left: 2
-                Input 4 digit code:
-                Number of correct digits in correct place: 1
-                Number of correct digits not in correct place: 3
-                Turns left: 1
-                Input 4 digit code:
-                Number of correct digits in correct place: 2
-                Number of correct digits not in correct place: 2
-                No more turns left.
-                The code was: 2345""",capturedOutput.toString().strip());
+        assertEquals("4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.\n"+
+                "Input 4 digit code:\n"+
+                "Please enter exactly 4 digits (each from 1 to 8).\n"+
+                "Input 4 digit code:\n"+
+                "Please enter exactly 4 digits (each from 1 to 8).\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "Turns left: 11\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "Turns left: 10\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 9\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 1\n"+
+                "Turns left: 8\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 2\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 7\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 1\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 6\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 5\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 1\n"+
+                "Turns left: 4\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 1\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "Turns left: 3\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 0\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "Turns left: 2\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 1\n"+
+                "Number of correct digits not in correct place: 3\n"+
+                "Turns left: 1\n"+
+                "Input 4 digit code:\n"+
+                "Number of correct digits in correct place: 2\n"+
+                "Number of correct digits not in correct place: 2\n"+
+                "No more turns left.\n"+
+                "The code was: 2345",capturedOutput.toString().strip());
 
     }
 }
